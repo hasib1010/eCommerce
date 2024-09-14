7f9d4aeb2c006b3451fd4bc413aaa9cae0b7e7cf
hasib1010 <asifmahmud664@gmail.com>
2024-09-14 10:51:38 +0600

    smooth scrolling added

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,46 +2,59 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Marquee from "react-fast-marquee";
 import NavBar from './Navbar';
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
+
 
 const Layout = () => {
+    const lenis = useLenis(({ scroll }) => {
+        // called every scroll
+    })
+
     return (
-        <div className='relative'>
-         
-            <div className='w-full  bg-[#8567e6]'>
-                <Marquee className='py-1'>
-                    <div className='flex text-with-outline'>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                        <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
-                    </div>
-                </Marquee>
+        <ReactLenis root
+        options={{
+            smooth: true,
+            duration: 1.8, 
+        }}
+        >
+            <div className='relative'>
+
+                <div className='w-full  bg-[#8567e6]'>
+                    <Marquee className='py-1'>
+                        <div className='flex text-with-outline'>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                            <h3 className='mr-32'>30% OFF - Offer Ends Tomorrow - Grab Now</h3>
+                        </div>
+                    </Marquee>
+                </div>
+
+                {/* Navbar - fixed to the top */}
+                <NavBar />
+
+                {/* Main Content */}
+                <div className=' '>
+                    <Outlet />
+                </div>
+
+                {/* Footer or other layout components */}
             </div>
-
-            {/* Navbar - fixed to the top */}
-            <NavBar />
-
-            {/* Main Content */}
-            <div className=' '>
-                <Outlet />
-            </div>
-
-            {/* Footer or other layout components */}
-        </div>
+        </ReactLenis>
     );
 }
 
